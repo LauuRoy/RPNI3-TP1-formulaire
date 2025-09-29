8ab2f2a631fd0d73c335dad0343912c72c9f442a
Laurie Roy <laurie.roy4@gmail.com>
2025-09-28 22:42:06 -0400

    Ajout validation + css

--- public/js/script.js
+++ public/js/script.js
@@ -2,32 +2,48 @@
 let boutonPrecedent = null;
 let boutonSuivant = null;
 let boutonSoumettre = null;
+let boutonModifier = null;
 let etape = 0;
 let fieldsets;
 let donFrequent = null;
+let donUnique = null;
 let labelFrequence = null;
 let selectFrequence = null;
 let etapeActuelle = null;
-//Fonctionnement des boutons
+let messagesJSON = {};
+// Fonctionnement des boutons
 function afficherEtape(index) {
     cacherFieldsets();
-    if (index >= 0 && index < fieldsets.length) {
-        fieldsets[index].classList.remove("cacher");
-    }
     if (index === 0) {
+        fieldsets[0]?.classList.remove("cacher");
+        fieldsets[1]?.classList.remove("cacher");
+        donUnique?.classList.remove("cacher");
+        donFrequent?.classList.remove("cacher");
         boutonPrecedent?.classList.add("cacher");
         boutonSuivant?.classList.remove("cacher");
         boutonSoumettre?.classList.add("cacher");
+        boutonModifier?.classList.add("cacher");
     }
-    else if (index < fieldsets.length - 1) {
+    else if (index < fieldsets.length - 2) {
+        fieldsets[index + 1]?.classList.remove("cacher");
         boutonPrecedent?.classList.remove("cacher");
         boutonSuivant?.classList.remove("cacher");
         boutonSoumettre?.classList.add("cacher");
+        boutonModifier?.classList.add("cacher");
     }
     else {
+        fieldsets[index + 1]?.classList.remove("cacher");
+        donUnique?.classList.add("cacher");
+        donFrequent?.classList.add("cacher");
         boutonPrecedent?.classList.remove("cacher");
         boutonSuivant?.classList.add("cacher");
         boutonSoumettre?.classList.remove("cacher");
+        if (index === 3) {
+            boutonModifier?.classList.remove("cacher");
+        }
+        else {
+            boutonModifier?.classList.add("cacher");
+        }
     }
 }
 function cacherFieldsets() {
@@ -35,11 +51,10 @@ function cacherFieldsets() {
         fieldset.classList.add("cacher");
     });
 }
-function navigerSuivant() {
-    if (etape < fieldsets.length - 1) {
-        etape++;
-        afficherEtape(etape);
-    }
+// Navigation
+function naviguerModifier() {
+    etape = 1;
+    afficherEtape(etape);
 }
 function navigerPrecedent() {
     if (etape > 0) {
@@ -47,6 +62,49 @@ function navigerPrecedent() {
         afficherEtape(etape);
     }
 }
+function navigerSuivant() {
+    const etapeValide = validation(etape);
+    if (etapeValide) {
+        if (etape < fieldsets.length - 2) {
+            etape++;
+            afficherEtape(etape);
+            if (etape === 3) {
+                remplirResume();
+            }
+        }
+    }
+}
+// Remplir le résumé
+function remplirResume() {
+    const formulaire = document.querySelector("form");
+    const typeDon = formulaire.querySelector("input[name=donBoutons]:checked")?.value;
+    const frequence = formulaire.querySelector("select[name=frequence]")?.value;
+    let montantFinal = formulaire.querySelector("input[name=montant]:checked")?.value || "";
+    if (!montantFinal) {
+        montantFinal = formulaire.querySelector("input[name=autreMontant]")?.value || "";
+    }
+    const prenom = formulaire.querySelector("input[name=prenom]")?.value;
+    const nom = formulaire.querySelector("input[name=nom]")?.value;
+    const telephone = formulaire.querySelector("input[name=telephone]")?.value;
+    const ville = formulaire.querySelector("input[name=ville]")?.value;
+    const pays = formulaire.querySelector("select[name=pays]")?.value;
+    const adresse = formulaire.querySelector("input[name=adresse]")?.value;
+    const codepostal = formulaire.querySelector("input[name=codepostal]")?.value;
+    const nomCarte = formulaire.querySelector("input[name=nomCarte]")?.value;
+    const carteCredit = formulaire.querySelector("input[name=carteCredit]")?.value;
+    document.getElementById("montantResume").textContent = montantFinal + "$";
+    document.getElementById("frequenceResume").textContent = typeDon === "donFrequent" ? frequence : "Don unique";
+    document.getElementById("prenomResume").textContent = prenom;
+    document.getElementById("nomResume").textContent = nom;
+    document.getElementById("telephoneResume").textContent = telephone;
+    document.getElementById("villeResume").textContent = ville;
+    document.getElementById("paysResume").textContent = pays;
+    document.getElementById("adresseResume").textContent = adresse;
+    document.getElementById("codepostalResume").textContent = codepostal;
+    document.getElementById("nomCarteResume").textContent = nomCarte;
+    document.getElementById("carteCreditResume").textContent = carteCredit ? `**** **** **** ${carteCredit.slice(-4)}` : "";
+}
+// Faire apparaitre fréquence
 function changerDonFrequent() {
     if (donFrequent?.checked) {
         labelFrequence?.classList.remove("cacher");
@@ -57,6 +115,66 @@ function changerDonFrequent() {
         selectFrequence?.classList.add("cacher");
     }
 }
+// Message d'erreurs et validation
+async function obtenirMessagesErreur() {
+    const reponse = await fetch('objJSONMessages.json');
+    messagesJSON = await reponse.json();
+}
+function validerChamps(champ) {
+    const id = champ.id;
+    const erreurElement = document.getElementById("erreur-" + id);
+    if (erreurElement)
+        erreurElement.textContent = "";
+    if (champ.validity.valueMissing && messagesJSON[id]?.vide) {
+        erreurElement.textContent = messagesJSON[id].vide;
+        return false;
+    }
+    if (champ.validity.typeMismatch && messagesJSON[id]?.type) {
+        erreurElement.textContent = messagesJSON[id].type;
+        return false;
+    }
+    if (champ.validity.patternMismatch && messagesJSON[id]?.pattern) {
+        erreurElement.textContent = messagesJSON[id].pattern;
+        return false;
+    }
+    return true;
+}
+function validation(etape) {
+    let etapeValide = true;
+    switch (etape) {
+        // information perso
+        case 1: {
+            const prenomElement = document.getElementById("prenom");
+            const nomElement = document.getElementById("nom");
+            const courrielElement = document.getElementById("courriel");
+            const prenomValide = validerChamps(prenomElement);
+            const nomValide = validerChamps(nomElement);
+            const courrielValide = validerChamps(courrielElement);
+            if (!prenomValide || !nomValide || !courrielValide) {
+                etapeValide = false;
+            }
+            break;
+        }
+        // Information paiement -- NE RENTRE PAS DANS LA BOUCLE?
+        case 2: {
+            const carteCreditElement = document.getElementById("carteCredit");
+            const nomCarteElement = document.getElementById("nomCarte");
+            const dateExpirationElement = document.getElementById("dateExpiration");
+            const cvvElement = document.getElementById("codeVerification");
+            const carteCreditValide = validerChamps(carteCreditElement);
+            const nomCarteValide = validerChamps(nomCarteElement);
+            const dateExpirationValide = validerChamps(dateExpirationElement);
+            const cvvValide = validerChamps(cvvElement);
+            if (!carteCreditValide || !nomCarteValide || !dateExpirationValide || !cvvValide) {
+                etapeValide = false;
+            }
+            break;
+        }
+    }
+    return etapeValide;
+}
+
+// Initialisation
 function initialiser() {
     const formulaire = document.querySelector("form");
     if (formulaire) {
@@ -65,25 +183,24 @@ function initialiser() {
     fieldsets = document.querySelectorAll("fieldset");
     boutonPrecedent = document.getElementById("precedent");
     boutonSuivant = document.getElementById("suivant");
+    boutonModifier = document.getElementById("modifier");
     boutonSoumettre = document.getElementById("soumettre");
     donFrequent = document.getElementById("donFrequent");
-    // Correction des bons sélecteurs
+    donUnique = document.getElementById("donUnique");
     labelFrequence = document.querySelector("label[for='frequence']");
     selectFrequence = document.getElementById("frequence");
-    const donUnique = document.getElementById("donUnique");
-    // Masquer la fréquence au départ
     labelFrequence?.classList.add("cacher");
     selectFrequence?.classList.add("cacher");
-    // Attacher les événements sur les deux radios
     donFrequent?.addEventListener("change", changerDonFrequent);
     donUnique?.addEventListener("change", changerDonFrequent);
-    changerDonFrequent(); // Appeler une fois pour initialiser l'état correct
+    changerDonFrequent();
     fieldsets.forEach((f) => f.classList.add("cacher"));
     boutonSoumettre?.classList.add("cacher");
     boutonPrecedent?.classList.add("cacher");
     boutonPrecedent?.addEventListener("click", navigerPrecedent);
     boutonSuivant?.addEventListener("click", navigerSuivant);
+    boutonModifier?.addEventListener("click", naviguerModifier);
+    obtenirMessagesErreur();
     afficherEtape(etape);
 }
 document.addEventListener("DOMContentLoaded", initialiser);
-//LORSUQE AUTRE MONTANT EST SELECTIONNER, LE PRIX COCHER EN BOUTON RADIO DOIT ETRE ENLEVER
--- src/ts/script.ts
+++ src/ts/script.ts
@@ -1,34 +1,58 @@
 let boutonPrecedent: HTMLInputElement | null = null;
 let boutonSuivant: HTMLInputElement | null = null;
 let boutonSoumettre: HTMLInputElement | null = null;
+let boutonModifier: HTMLInputElement | null = null;
 let etape: number = 0;
 let fieldsets: NodeListOf<HTMLElement>;
 let donFrequent: HTMLInputElement | null = null;
+let donUnique: HTMLInputElement | null = null;
 let labelFrequence: HTMLElement | null = null;
 let selectFrequence: HTMLSelectElement | null = null;
 let etapeActuelle: HTMLElement | null = null;
 
+interface ErreurJSON {
+    vide: string;
+    pattern: string;
+    type: string;
+}
+let messagesJSON: Record<string, ErreurJSON> = {};
 
-//Fonctionnement des boutons
+
+// Fonctionnement des boutons
 function afficherEtape(index: number): void {
     cacherFieldsets();
-    
-    if (index >= 0 && index < fieldsets.length) {
-        fieldsets[index].classList.remove("cacher");
-    }
 
     if (index === 0) {
+
+        fieldsets[0]?.classList.remove("cacher");
+        fieldsets[1]?.classList.remove("cacher");
+        donUnique?.classList.remove("cacher");
+        donFrequent?.classList.remove("cacher");
         boutonPrecedent?.classList.add("cacher");
         boutonSuivant?.classList.remove("cacher");
         boutonSoumettre?.classList.add("cacher");
-    } else if (index < fieldsets.length - 1) {
+        boutonModifier?.classList.add("cacher");
+    } else if (index < fieldsets.length - 2) {
+
+        fieldsets[index + 1]?.classList.remove("cacher"); 
         boutonPrecedent?.classList.remove("cacher");
         boutonSuivant?.classList.remove("cacher");
         boutonSoumettre?.classList.add("cacher");
+        boutonModifier?.classList.add("cacher");
     } else {
+
+        fieldsets[index + 1]?.classList.remove("cacher");
+        donUnique?.classList.add("cacher");
+        donFrequent?.classList.add("cacher");
         boutonPrecedent?.classList.remove("cacher");
         boutonSuivant?.classList.add("cacher");
         boutonSoumettre?.classList.remove("cacher");
+        
+        if (index === 3) {
+            boutonModifier?.classList.remove("cacher");
+        } else {
+            boutonModifier?.classList.add("cacher");
+        }
     }
 }
 
@@ -38,13 +62,11 @@ function cacherFieldsets(): void {
     });
 }
 
-function navigerSuivant(): void {
-    if (etape < fieldsets.length - 1) {
-        etape++;
-        afficherEtape(etape);
-    }
+// Navigation
+function naviguerModifier(): void {
+    etape = 1;
+    afficherEtape(etape);
 }
-
 
 function navigerPrecedent(): void {
     if (etape > 0) {
@@ -53,7 +75,57 @@ function navigerPrecedent(): void {
     }
 }
 
-//Faire apparaitre fréquence
+function navigerSuivant(): void {
+    const etapeValide = validation(etape);
+
+    if (etapeValide) {
+        if (etape < fieldsets.length - 2) {
+            etape++;
+            afficherEtape(etape);
+
+            if (etape === 3) {
+                remplirResume();
+            }
+        }
+    }
+}
+
+// Remplir le résumé
+function remplirResume(): void {
+    const formulaire = document.querySelector("form") as HTMLFormElement;
+    const typeDon = (formulaire.querySelector("input[name=donBoutons]:checked") as HTMLInputElement)?.value;
+    const frequence = (formulaire.querySelector("select[name=frequence]") as HTMLSelectElement)?.value;
+
+    let montantFinal = (formulaire.querySelector("input[name=montant]:checked") as HTMLInputElement)?.value || "";
+    if (!montantFinal) {
+        montantFinal = (formulaire.querySelector("input[name=autreMontant]") as HTMLInputElement)?.value || "";
+    }
+
+    const prenom = (formulaire.querySelector("input[name=prenom]") as HTMLInputElement)?.value;
+    const nom = (formulaire.querySelector("input[name=nom]") as HTMLInputElement)?.value;
+    const telephone = (formulaire.querySelector("input[name=telephone]") as HTMLInputElement)?.value;
+    const ville = (formulaire.querySelector("input[name=ville]") as HTMLInputElement)?.value;
+    const pays = (formulaire.querySelector("select[name=pays]") as HTMLSelectElement)?.value;
+    const adresse = (formulaire.querySelector("input[name=adresse]") as HTMLInputElement)?.value;
+    const codepostal = (formulaire.querySelector("input[name=codepostal]") as HTMLInputElement)?.value;
+    const nomCarte = (formulaire.querySelector("input[name=nomCarte]") as HTMLInputElement)?.value;
+    const carteCredit = (formulaire.querySelector("input[name=carteCredit]") as HTMLInputElement)?.value;
+
+    document.getElementById("montantResume")!.textContent = montantFinal + "$";
+    document.getElementById("frequenceResume")!.textContent = typeDon === "donFrequent" ? frequence : "Don unique";
+    document.getElementById("prenomResume")!.textContent = prenom;
+    document.getElementById("nomResume")!.textContent = nom;
+    document.getElementById("telephoneResume")!.textContent = telephone;
+    document.getElementById("villeResume")!.textContent = ville;
+    document.getElementById("paysResume")!.textContent = pays;
+    document.getElementById("adresseResume")!.textContent = adresse;
+    document.getElementById("codepostalResume")!.textContent = codepostal;
+    document.getElementById("nomCarteResume")!.textContent = nomCarte;
+    document.getElementById("carteCreditResume")!.textContent = carteCredit ? `**** **** **** ${carteCredit.slice(-4)}` : "";
+}
+
+
+// Faire apparaitre fréquence
 function changerDonFrequent(): void {
     if (donFrequent?.checked) {
         labelFrequence?.classList.remove("cacher");
@@ -64,7 +136,81 @@ function changerDonFrequent(): void {
     }
 }
 
+// Message d'erreurs et validation
+async function obtenirMessagesErreur(): Promise<void> {
+    const reponse = await fetch('objJSONMessages.json');
+    messagesJSON = await reponse.json();
+}
 
+function validerChamps(champ: HTMLInputElement): boolean {
+    const id = champ.id;
+    const erreurElement = document.getElementById("erreur-" + id) as HTMLElement;
+
+    if (erreurElement) erreurElement.textContent = "";
+
+    if (champ.validity.valueMissing && messagesJSON[id]?.vide) {
+        erreurElement.textContent = messagesJSON[id].vide;
+        return false;
+    }
+
+    if (champ.validity.typeMismatch && messagesJSON[id]?.type) {
+        erreurElement.textContent = messagesJSON[id].type;
+        return false;
+    }
+
+    if (champ.validity.patternMismatch && messagesJSON[id]?.pattern) {
+        erreurElement.textContent = messagesJSON[id].pattern;
+        return false;
+    }
+
+    return true;
+}
+
+function validation(etape: number): boolean {
+    let etapeValide = true;
+
+    switch (etape) {
+
+        // information perso
+        case 1: {
+            const prenomElement = document.getElementById("prenom") as HTMLInputElement;
+            const nomElement = document.getElementById("nom") as HTMLInputElement;
+            const courrielElement = document.getElementById("courriel") as HTMLInputElement;
+
+            const prenomValide = validerChamps(prenomElement);
+            const nomValide = validerChamps(nomElement);
+            const courrielValide = validerChamps(courrielElement);
+
+            if (!prenomValide || !nomValide || !courrielValide) {
+                etapeValide = false;
+            }
+            break;
+        }
+
+        // Information paiement -- NE RENTRE PAS DANS LA BOUCLE?
+        case 2: {
+            const carteCreditElement = document.getElementById("carteCredit") as HTMLInputElement;
+            const nomCarteElement = document.getElementById("nomCarte") as HTMLInputElement;
+            const dateExpirationElement = document.getElementById("dateExpiration") as HTMLInputElement;
+            const cvvElement = document.getElementById("codeVerification") as HTMLInputElement;
+
+            const carteCreditValide = validerChamps(carteCreditElement);
+            const nomCarteValide = validerChamps(nomCarteElement);
+            const dateExpirationValide = validerChamps(dateExpirationElement);
+            const cvvValide = validerChamps(cvvElement);
+
+            if (!carteCreditValide || !nomCarteValide || !dateExpirationValide || !cvvValide) {
+                etapeValide = false;
+            }
+            break;
+        }
+    }
+
+    return etapeValide;
+}
+
+
+// Initialisation
 function initialiser(): void {
     const formulaire = document.querySelector("form") as HTMLFormElement;
     if (formulaire) {
@@ -74,36 +220,32 @@ function initialiser(): void {
     fieldsets = document.querySelectorAll("fieldset");
     boutonPrecedent = document.getElementById("precedent") as HTMLInputElement;
     boutonSuivant = document.getElementById("suivant") as HTMLInputElement;
+    boutonModifier = document.getElementById("modifier") as HTMLInputElement;
     boutonSoumettre = document.getElementById("soumettre") as HTMLInputElement;
     donFrequent = document.getElementById("donFrequent") as HTMLInputElement;
-
+    donUnique = document.getElementById("donUnique") as HTMLInputElement;
 
     labelFrequence = document.querySelector("label[for='frequence']") as HTMLElement;
     selectFrequence = document.getElementById("frequence") as HTMLSelectElement;
-    const donUnique = document.getElementById("donUnique") as HTMLInputElement;
 
-    
     labelFrequence?.classList.add("cacher");
     selectFrequence?.classList.add("cacher");
 
-    
     donFrequent?.addEventListener("change", changerDonFrequent);
     donUnique?.addEventListener("change", changerDonFrequent);
-    changerDonFrequent(); 
-
+    changerDonFrequent();
 
     fieldsets.forEach((f) => f.classList.add("cacher"));
     boutonSoumettre?.classList.add("cacher");
     boutonPrecedent?.classList.add("cacher");
-    
 
     boutonPrecedent?.addEventListener("click", navigerPrecedent);
     boutonSuivant?.addEventListener("click", navigerSuivant);
+    boutonModifier?.addEventListener("click", naviguerModifier);
 
+    obtenirMessagesErreur();
     afficherEtape(etape);
+    
 }
 
 document.addEventListener("DOMContentLoaded", initialiser);
-
-
-//LORSUQE AUTRE MONTANT EST SELECTIONNER, LE PRIX COCHER EN BOUTON RADIO DOIT ETRE ENLEVER
